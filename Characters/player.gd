21a984b79ee4e9e0b7f9eef59d1744145759804b
PROFLEBOURGEOIS <fabduroule@live.fr>
2025-01-18 14:51:49 +0100

    idle works

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,25 +1,37 @@
 extends CharacterBody2D
 
+@export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0,1)
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
+#parameters/Idle/blend_position
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _ready():
+	animation_tree.set("parameters/Idle/blend_position", starting_direction)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(_delta):
+	
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	print(velocity);
+	update_animation_parameters(input_direction)
+	velocity = input_direction * move_speed
 	move_and_slide()
+	pick_new_state()
+	
+	
+func update_animation_parameters(move_input : Vector2):
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+		
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
